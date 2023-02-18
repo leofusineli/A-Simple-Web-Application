71cf1465f04e81af06dc77fe5dbe6be1fc1cf53f
lsilva <leonardo.silva@ppro.com>
2023-02-18 18:00:21 -0300

    Creating Application Initializer

---
+++ src/main/java/com/test/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override public void onStartup(final ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        ServletRegistration.Dynamic servletRegistration =
+                servletContext.addServlet("mvc", new DispatcherServlet(context));
+
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+    }
+}
